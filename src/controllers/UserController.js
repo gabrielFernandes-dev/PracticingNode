e27555d085e83346b3ce8f19c258ad4b4ce33713
Gabriel Fernandes <gabriel.work076@gmail.com>
2021-09-29 12:12:51 -0300

    [UserController] GET /users findAll

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,21 @@
 const User = require('../models/Users');
 
 module.exports = {
-  
+  async findAll(req, res) {
+    try {
+      const filter = {
+        limit: req.query.limit || 10,
+        offset: req.query.offset || 0,
+      };
+      const users = await User.findAll(filter);
+      res.status(200).json({
+        status: 'success',
+        data: users,
+        total: users.length,
+      });
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
+const UserController = require('./controllers/UserController');
+
 const routes = require('express').Router();
 
 routes.route('/').get((_req, res) => {
   res.status(200).json({ message: 'Root route works!' });
 });
+
+routes
+  .route('/users')
+  .get(UserController.findAll);
 
 module.exports = routes;
