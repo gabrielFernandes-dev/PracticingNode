070190a84c5e98f4d6093efc5ce4510607cfa90e
Gabriel Fernandes <gabriel.work076@gmail.com>
2021-09-29 12:09:42 -0300

    db config ajdusts

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,12 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
+/*
+ * Models
+ * */
+const User = require('../models/Users');
 const Connection = new Sequelize(dbConfig);
+
+User.init(Connection);
+
 module.exports = Connection;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('./database');
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
