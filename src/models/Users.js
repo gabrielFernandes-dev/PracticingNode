15f019a3bb23e62fde6e0eee91ce349efec7deff
Gabriel Fernandes <gabriel.work076@gmail.com>
2021-09-29 12:09:54 -0300

    create User model

---
+++ src/models/Users.js
@@ -0,0 +1,18 @@
+const { Model, DataTypes } = require('sequelize');
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        job: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+module.exports = User;
