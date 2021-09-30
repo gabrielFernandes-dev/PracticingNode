5bc4960f372645228102a3a9d7572d65b556e831
Gabriel Fernandes <gabriel.work076@gmail.com>
2021-09-29 23:03:32 -0300

    add authorization middleware

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -64,4 +64,15 @@ module.exports = {
       });
     });
   },
+
+  async authorize(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader?.split(' ')[1];
+    if (!token) return res.sendStatus(401);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, _) => {
+      if (err) return res.sendStatus(403);
+      console.log('Authorized.');
+      next();
+    });
+  },  
 };
