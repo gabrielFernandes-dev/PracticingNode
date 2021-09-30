2c03c44de040f03eaf26b967d0a401973a8992fc
Gabriel Fernandes <gabriel.work076@gmail.com>
2021-09-30 01:48:23 -0300

    little refactoring

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -66,7 +66,7 @@ module.exports = {
     });
   },
 
-  async authorize(req, res, next) {
+  authorize(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader?.split(' ')[1];
     if (!token) return res.sendStatus(401);
