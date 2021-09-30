65dd69cb91fc58ca7dd4fd4529c99cf281008cff
Gabriel Fernandes <gabriel.work076@gmail.com>
2021-09-29 23:20:30 -0300

    add authServer global prefix and routes setup

--- src/authServer.js
+++ src/authServer.js
@@ -3,10 +3,17 @@ require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const router = express.Router();
+const AuthHandler = require('./middlewares/Auth');
 
 app.use(morgan('dev'));
 app.use(express.json());
 
+router.post('/login', AuthHandler.authenticate);
+router.post('/token', AuthHandler.refreshAccessToken);
+router.delete('/logout', AuthHandler.deauthenticate);
+
+app.use('/api', router);
 
 app.listen(4000, () => {
   console.log(`Auth server running on http://localhost:4000/`);
